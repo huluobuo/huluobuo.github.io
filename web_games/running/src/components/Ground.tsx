57c3ee36c3f702dd9ac7713bfeaeb4e7e7dbbb16
huluobuo <HelloHuHi@outlook.com>
2024-10-20 17:01:42 +0800

    Delete web_games/running directory

--- web_games/running/src/components/Ground.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const Ground: React.FC = () => {
-  return (
-    <div className="absolute bottom-0 left-0 w-full h-10 bg-brown-500"></div>
-  );
-};
-
-export default Ground;
